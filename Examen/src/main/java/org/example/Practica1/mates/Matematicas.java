01b66db69fe8252935b65056784873920ceff0b6
DavidPeinadoD <davidpeinadodiaz@gmail.com>
2023-03-23 19:45:02 +0100

    Clase principal copiada del enunciado del examen, variables iniciadas en tablero y metodos creados vacios

--- Examen/src/main/java/org/example/Practica1/mates/Matematicas.java
+++ Examen/src/main/java/org/example/Practica1/mates/Matematicas.java
@@ -9,19 +9,19 @@ public class Matematicas {
      * El parámetro ‘pasos‘ indica el número de puntos generado.
      */
     public static double generarNumeroPi(long pasos) {
-        double aciertos = 0;
-        double areaCuadrado = 4;
+        double aciertos =0;
+        double areaCuadrado =4;
 
-        for (long i = 0; i < pasos; i++) {
-            double x = Math.random() * 2 - 1; // Genera un número aleatorio entre -1 y 1
-            double y = Math.random() * 2 - 1; // Genera un número aleatorio entre -1 y 1
+        for (long i = 0; i < pasos; i++ ) {
+            double x = Math.random() *2 - 1; // Genera un número aleatorio entre -1 y 1
+            double y = Math.random() *2 - 1; // Genera un número aleatorio entre -1 y 1
 
-            if (Math.sqrt(x * x + y * y) <= 1) { // Comprueba si el punto está dentro del círculo del radio definido
-                aciertos++;
+            if (Math.sqrt(x * x + y * y) <= 1) {// Comprueba si el punto está dentro del círculo del radio definido
+                 aciertos++;
             }
         }
 
-        return areaCuadrado * (aciertos / pasos);
+         return areaCuadrado * (aciertos / pasos);
     }
 
 }
--- Examen/src/main/java/org/example/Practica2/dominio/Tablero.java
+++ Examen/src/main/java/org/example/Practica2/dominio/Tablero.java
@@ -1,4 +1,29 @@
 package org.example.Practica2.dominio;
 
 public class Tablero {
+    private static int DIMENSION = 30;
+    private int[][] estadoActual;
+    private int[][] estadoSiguiente = new int[DIMENSION][DIMENSION];
+
+    public void leerEstadoActual(){
+
+
+
+    }
+    public void generarEstadoActualPorMontecarlo() {
+    }
+    public void transitarAlEstadoSiguiente(){
+
+    }
+
+
+    @Override
+    public String toString(){
+        return ""; // Esta línea hay que modificarla.
+    }
+
+
 }
+
+
+
